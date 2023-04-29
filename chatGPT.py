0c176181b1d9381f755186ad3089b7317a19a2eb
Nilanjit Chakraborty <chakrabortynilanjit@gmail.com>
2023-04-29 23:34:40 +0530

    chatGPT on terminal

---
+++ chatGPT.py
@@ -0,0 +1,18 @@
+import openai
+openai.api_key = "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"
+while True:
+    model_engine = "text-davinci-003"
+    prompt = input('Enter new prompt: ')
+    if 'x' in prompt: # Exit condition
+        break
+    # Generate a response
+    completion = openai.Completion.create( # Create completion object
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    response = completion.choices[0].text # Get response
+    print(response)
